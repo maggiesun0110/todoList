5bafc12505a9b997de75afc2928850bad04f68b5
Maggie <maggiesun625@gmail.com>
2025-11-30 19:49:32 -0800

    continuing starting of js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,21 @@
-function addNewList(){
+let signature = document.getElementById("signature");
+let currentList = document.getElementById("schoolList");
+let lists = [document.getElementById("schoolList"), document.getElementById("dailyNecessitiesList")];
+let currListIndex = 0;
 
+//add the customizability of the name later
+function addNewList(){
+    signature.innerHTML += `
+        <h2 id = "${name}">new list</h2>
+    `;
+    lists.push(`new list`);
 }
 
 function deleteList(){
-
+    lists.splice(currListIndex, 1);
+    if (currListIndex >= lists.length){
+        currListIndex = lists.length - 1;
+    }
 }
 
 function previousList(){
@@ -11,7 +23,12 @@ function previousList(){
 }
 
 function nextList(){
-
+    currListIndex += 1;
+    if(currListIndex >= lists.length){
+        currListIndex = 0;
+    }
+    currentList = lists[currListIndex+1];
+    signature
 }
 
 function addTodo(){
@@ -19,5 +36,5 @@ function addTodo(){
 }
 
 function motivateMe(){
-    
+
 }
